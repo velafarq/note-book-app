a48cbdc93b2358fa20c06a5ebd1b28f194e68575
velafarq <vela.mrdjen@gmail.com>
2018-07-03 22:05:22 -0500

    Fix bug in register

--- public/js/login.controller.js
+++ public/js/login.controller.js
@@ -1,5 +1,6 @@
 const login = e => {
     e.preventDefault();
+    
   
     const email = $("#login-user").find('#user-email').val();
     const password = $("#login-user").find('#user-password').val();
@@ -8,7 +9,7 @@ const login = e => {
       password,
     });
     
-    fetch('http://localhost:8080/auth/login', {
+    fetch(`${BASE_URL}/auth/login`, {
       method: 'POST',
       body: data,
       headers: {
@@ -19,8 +20,7 @@ const login = e => {
       .then(data => {
         
         localStorage.setItem('token', data.token);
-        $(".main__block").empty();
-        getData(displayRecentPosts);
+
       })
       .catch(error => {
         $(".main__block").empty();
@@ -28,6 +28,7 @@ const login = e => {
           <p>Oops, your login was not successful!</p>
          `);
       });
+      getData(displayRecentPosts);
   };
   
   const register = e => {
@@ -35,23 +36,23 @@ const login = e => {
   
     const email = $("#register-new-user").find('#user-email').val();
     const password = $("#register-new-user").find('#user-password').val();
-    const data = new FormData();
+    const data = JSON.stringify({
+      email,
+      password,
+    });
   
-    data.append('json', JSON.stringify({ "email": email, "password": password }));
-      
+    
     fetch(`${BASE_URL}/auth/register`, {
       method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-      },
       body: data,
-        
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
       .then(res => res.json())
-      .then(userInfo => {
+      .then(data => {
       
-        console.log(userInfo);
-        localStorage.setItem('token', userInfo.token);
+        localStorage.setItem('token', data.token);
         $(".main__block").empty();
         $(".main__block").html(`Welcome! Click <a href="#" onlick="goToNewEntry()">here</a>to begin a new post.`)
         
