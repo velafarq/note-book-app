0c5cfbbc3d309da15e94ec425623b3d806ca808d
velafarq <vela.mrdjen@gmail.com>
2018-10-02 12:32:09 -0500

    Add sort to find query

--- api/post/post.controller.js
+++ api/post/post.controller.js
@@ -1,123 +1,125 @@
-const { Post } = require('./post.model');
+const { Post } = require("./post.model");
 
 const getPosts = (req, res) => {
   const { user } = req;
-  Post
-      .find({
-          author: user._id
-      })
-      .then(posts => {
-          res.json(posts);
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({
-              error: 'something went wrong'
-          });
+  Post.find({
+    author: user._id
+  })
+    .sort([["date", -1]])
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: "something went wrong"
       });
+    });
 };
 
 const getPostById = (req, res) => {
   const { user } = req;
-  Post
-      .findOne({
-          _id: req.params.id,
-          author: user._id
-      })
-      .then(post => {
-          res.json(post);
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({
-              error: 'something went wrong'
-          });
+  Post.findOne({
+    _id: req.params.id,
+    author: user._id
+  })
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: "something went wrong"
       });
-}
+    });
+};
 
 const createPost = (req, res) => {
-  const requiredFields = ['title', 'content', 'category'];
+  const requiredFields = ["title", "content", "category"];
   const { user } = req;
-  console.log('hellooooo', user);
+  console.log("hellooooo", user);
   for (let i = 0; i < requiredFields.length; i++) {
-      const field = requiredFields[i];
-      if (!(field in req.body)) {
-          const message = `Missing \`${field}\` in request body`;
-          console.error(message);
-          return res.status(400).send(message);
-      }
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
   }
 
-  Post
-      .create({
-          author: user._id,
-          title: req.body.title,
-          content: req.body.content,
-          category: req.body.category
+  Post.create({
+    author: user._id,
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category
+  })
+    .then(post =>
+      res.status(201).json({
+        _id: post._id
       })
-      .then(post => res.status(201).json({
-          _id: post._id
-      }))
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({
-              error: 'Something went wrong'
-          });
+    )
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: "Something went wrong"
       });
+    });
 };
 
 const updatePost = (req, res) => {
   const { user } = req;
 
   if (!(req.params.id && req.body._id && req.params.id === req.body._id)) {
-      res.status(400).json({
-          error: 'Request path id and request body id values must match'
-      });
+    res.status(400).json({
+      error: "Request path id and request body id values must match"
+    });
   }
 
   const updated = {};
-  const updateableFields = ['title', 'content', 'category'];
+  const updateableFields = ["title", "content", "category"];
   updateableFields.forEach(field => {
-      if (field in req.body) {
-          updated[field] = req.body[field];
-      }
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
   });
-  Post
-      .findOneAndUpdate({
-          _id: req.params.id,
-          author: user._id
-      }, {
-          $set: updated
-      }, {
-          new: true
-      })
-      .then(updatedPost => res.status(204).end())
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({
-              error: 'Something went wrong'
-          });
+  Post.findOneAndUpdate(
+    {
+      _id: req.params.id,
+      author: user._id
+    },
+    {
+      $set: updated
+    },
+    {
+      new: true
+    }
+  )
+    .then(updatedPost => res.status(204).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: "Something went wrong"
       });
-}
+    });
+};
 
 const deletePost = (req, res) => {
   const { user } = req;
-  Post
-      .findOneAndRemove({
-          _id: req.params.id,
-          author: user._id
-      })
-      .then(() => {
-          res.status(200).json({
-              message: 'Post successfully deleted'
-          });
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({
-              error: 'something went wrong'
-          });
+  Post.findOneAndRemove({
+    _id: req.params.id,
+    author: user._id
+  })
+    .then(() => {
+      res.status(200).json({
+        message: "Post successfully deleted"
       });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: "something went wrong"
+      });
+    });
 };
 
-  module.exports = { getPosts, getPostById, createPost, updatePost, deletePost };
+module.exports = { getPosts, getPostById, createPost, updatePost, deletePost };
--- public/js/post.view.js
+++ public/js/post.view.js
@@ -1,17 +1,16 @@
-
-
-
-const displayRecentPosts = (data) => {
+const displayRecentPosts = data => {
   loggedInView();
   $(".main__block").empty();
   for (index in data) {
-   let date = new Date(data[index].created);
-    date.toDateString(); 
-    $("#category_results").html(`<h3> My Posts</h3>`)
-     
-      $(".main__block").append(
-          ` <section class="main__block__post-summary">
-      <div class="post-summary__heading"><i class="fas fa-bookmark"></i><h3 class="main__block__post-summary__summary-heading">${data[index].title}</h3></div>
+    let date = new Date(data[index].created);
+    date.toDateString();
+    $("#category_results").html(`<h3> My Posts</h3>`);
+
+    $(".main__block").append(
+      ` <section class="main__block__post-summary">
+      <div class="post-summary__heading"><i class="fas fa-bookmark"></i><h3 class="main__block__post-summary__summary-heading">${
+        data[index].title
+      }</h3></div>
       <p class="main__main__block__post-summary__date">${date}</p>
       <p class="main__block__post-summary__info">In: ${data[index].category}</p>
       <p class="main__block__post-summary-snippet">${data[index].content}</p>
@@ -19,36 +18,37 @@ const displayRecentPosts = (data) => {
         data[index]._id
       }')">VIEW MORE <i class="fas fa-caret-right"></i></a>
   </section>`
-      );
+    );
   }
-}
-
+};
 
 const categoryFilter = (data, specificCategory) => {
   const categoryData = data.filter(post => post.category === specificCategory);
   loggedInView();
   $(".main__block").empty();
-  $("#category_results").html(`<h3> My ${specificCategory}</h3>`)
+  $("#category_results").html(`<h3> My ${specificCategory}</h3>`);
 
   for (index in categoryData) {
-    
-      $(".main__block").append(`<section class="main__block__post-summary">
-      <h3 class="main__block__post-summary__summary-heading">${categoryData[index].title}</h3>
+    $(".main__block").append(`<section class="main__block__post-summary">
+      <h3 class="main__block__post-summary__summary-heading">${
+        categoryData[index].title
+      }</h3>
       <p class="main__block__post-summary__info">In: ${
-        categoryData[index].category}</p>
-      <p class="main__block__post-summary-snippet">${categoryData[index].content}</p>
+        categoryData[index].category
+      }</p>
+      <p class="main__block__post-summary-snippet">${
+        categoryData[index].content
+      }</p>
       <a class="main__block__post-summary__view-more" onclick="getPostById('${
         categoryData[index]._id
       }')">VIEW MORE <i class="fas fa-caret-right"></i></a>
   </section>`);
   }
-}
-
-
+};
 
 const goToNewEntry = () => {
   loggedInView();
   $(".main__block").html(new_entry);
   $("#category_results").empty();
-  $('#addPost').submit(addPost);
+  $("#addPost").submit(addPost);
 };
