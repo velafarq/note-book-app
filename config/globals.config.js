7ca80d6441ce4e0b71cb1af5aaf34d3b1bbf28d6
Vela Farquharson <vela.mrdjen@gmail.com>
2022-06-13 17:48:47 -0500

    Update package lock and add db url for running locally

--- config/globals.config.js
+++ config/globals.config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL =
-  process.env.DATABASE_URL ||
-  "mongodb://velafarq:watermelon1@ds263590.mlab.com:63590/note-book-app";
+  process.env.DB_CONNECTION_URL || "mongodb://re34ZDBObWOHvWL3:TyWcv6nX92t3BYjn@vela-notebook-db-2-6nihn-mongodb.external.kinsta.app:32455/vela-notebook-db-2"
+
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
   "mongodb://velafarq:watermelon1@ds129801.mlab.com:29801/note-book-app-test";
